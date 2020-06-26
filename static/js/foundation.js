5486828006038013b7db2f659cd4c89f1df43600
xzx <563905150@qq.com>
2020-06-26 18:57:46 +0800

    规范修改

--- static/js/foundation.js
+++ static/js/foundation.js
@@ -1,41 +1,35 @@
-$(function (){
+$(function () {
 
-    var $tabsContent = $(".foundation-tabs-content .foundation-tab-pane");
-    var $tabsContentTitle = $(".foundation-tabs-content .foundation-tab-pane .foundation-tab-pane-title");
-    var $tabsTitle = $(".foundation-tabs-content .foundation-tabs");
-    $tabsTitle.find("li").eq(0).addClass("foundation-item-active");
+    var $tabsContent = $('.foundation-tabs-content .foundation-tab-pane');
+    var $tabsContentTitle = $('.foundation-tabs-content .foundation-tab-pane .foundation-tab-pane-title');
+    var $tabsTitle = $('.foundation-tabs-content .foundation-tabs');
+
+    $tabsTitle.find('li').eq(0).addClass('foundation-item-active');
     $tabsContent.hide().eq(0).show();
-    $tabsTitle.find("li").on("click", function (){
-        $(this).addClass("foundation-item-active").siblings().removeClass("foundation-item-active");
+
+    $tabsTitle.find('li').on('click', function () {
+        $(this).addClass('foundation-item-active').siblings().removeClass('foundation-item-active');
         $tabsContent.hide().eq($(this).index()).show();
     })
 
-    $tabsContent.eq(0).find(".foundation-tab-pane-title span").addClass("inner-arrow-down");
-    if($(window).width() < 978){
-        $tabsContent.show().eq(0).siblings().find(".foundation-tab-pane-title").siblings().hide();
+    $tabsContent.eq(0).find('.foundation-tab-pane-title span').addClass('inner-arrow-down');
+
+    if ($(window).width() < 978) {
+        $tabsContent.show().eq(0).siblings().find('.foundation-tab-pane-title').siblings().hide();
     }
-    // $(window).resize(function (){
 
-    //     if($(window).width() < 978){
-    //         if(!$("#search").find(".book-search").length){
-    //             $("#search").append($(".nav-search-input .book-search"));  
-    //         }
-            
-    //     }
-
-    // })
-    $tabsContentTitle.on("click", function (){
-        if(!$(this).find("span").hasClass("inner-arrow-down")){
-            $(this).find("span").addClass("inner-arrow-down")
-            .closest(".foundation-tab-pane")
-            .find(".inner").show()
-            .closest(".foundation-tab-pane")
-            .siblings()
-            .find(".foundation-tab-pane-title span")
-            .removeClass("inner-arrow-down")
-            .closest(".foundation-tab-pane-title")
-            .siblings()
-            .hide();
+    $tabsContentTitle.on('click', function () {
+        if (!$(this).find('span').hasClass('inner-arrow-down')) {
+            $(this).find('span').addClass('inner-arrow-down')
+                .closest('.foundation-tab-pane')
+                .find('.inner').show()
+                .closest('.foundation-tab-pane')
+                .siblings()
+                .find('.foundation-tab-pane-title span')
+                .removeClass('inner-arrow-down')
+                .closest('.foundation-tab-pane-title')
+                .siblings()
+                .hide();
         }
     })
 
