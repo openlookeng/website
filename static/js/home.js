fd8bb23b4c9b2b68cf1569bd827efdd989500876
Leslie-L <969966273@qq.com>
2020-11-13 14:33:04 +0800

    需求开发

--- static/js/home.js
+++ static/js/home.js
@@ -13,4 +13,25 @@ $(function () {
 
         }
     });
+
+    var myBannerSwiper = new Swiper ('.banner_swiper', {
+        direction: 'horizontal', // 切换选项
+        /* loop: true, */ // 循环模式选项
+        autoplay: {
+            delay: 5000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+        },
+
+        // 如果需要分页器
+        pagination: {
+            el: '.banner_swiper_pagination',
+            type: 'bullets',
+            clickable :true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    })
 });
