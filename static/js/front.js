a1425a0dcf26fe9666ab99b8e6c4178124a5c26e
zhengyuhanghans <zhengyuhang@huawei.com>
2021-06-23 21:28:20 +0800

    optimize resource loading.

--- static/js/front.js
+++ static/js/front.js
@@ -1,144 +1,12 @@
 /* global $this: true */
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "animationsSlider" }] */
-
-if ($.cookie('themeCSSpath')) {
-  $('link#theme-stylesheet').attr('href', $.cookie('themeCSSpath'))
-}
-if ($.cookie('themeLayout')) {
-  $('body').addClass($.cookie('themeLayout'))
-}
-
 $(function () {
-  sliderHomepage()
-  sliders()
   fullScreenContainer()
   productDetailGallery(4000)
   menuSliding()
   productDetailSizes()
   utils()
-  animations()
-  counters()
-  demo()
-  contactFormAjax()
 })
-
-// Ajax contact
-function contactFormAjax () {
-  var form = $('.contact-form-ajax')
-  if (typeof form === 'undefined') return false
-  form.submit(function () {
-    $this = $(this)
-    $.post($(this).attr('action'),
-      $this.serialize(),
-      function () {
-        $this[0].reset() // clear form
-
-        $('#contact-message')
-          .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
-          .fadeIn()
-      }
-      , 'json')
-    return false
-  })
-}
-
-/* for demo purpose only - can be deleted */
-function demo () {
-  if ($.cookie('themeCSSpath')) {
-    $('link#theme-stylesheet').attr('href', $.cookie('themeCSSpath'))
-  }
-
-  $('#colour').change(function () {
-    if ($(this).val() !== '') {
-      var themeCSSpath = 'css/style.' + $(this).val() + '.css'
-
-      $('link#theme-stylesheet').attr('href', themeCSSpath)
-
-      $.cookie('themeCSSpath', themeCSSpath, {expires: 365, path: '/'})
-    }
-
-    return false
-  })
-
-  $('#layout').change(function () {
-    if ($(this).val() !== '') {
-      var themeLayout = $(this).val()
-
-      $('body').removeClass('wide')
-      $('body').removeClass('boxed')
-
-      $('body').addClass(themeLayout)
-
-      $.cookie('themeLayout', themeLayout, {expires: 365, path: '/'})
-    }
-
-    return false
-  })
-}
-
-/* slider homepage */
-function sliderHomepage () {
-  if ($('#slider').length) {
-    // var owl = $('#slider')
-
-    $('#slider').owlCarousel({
-      autoPlay: 3000,
-      items: 4,
-      itemsDesktopSmall: [900, 3],
-      itemsTablet: [600, 3],
-      itemsMobile: [500, 2]
-    })
-  }
-}
-
-/* sliders */
-function sliders () {
-  if ($('.owl-carousel').length) {
-    $('.customers').owlCarousel({
-      items: 6,
-      itemsDesktopSmall: [990, 4],
-      itemsTablet: [768, 2],
-      itemsMobile: [480, 1]
-    })
-
-    $('.testimonials').owlCarousel({
-      items: 4,
-      itemsDesktopSmall: [990, 3],
-      itemsTablet: [768, 2],
-      itemsMobile: [480, 1]
-    })
-
-    $('.project').owlCarousel({
-      navigation: true, // Show next and prev buttons
-      navigationText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-      slideSpeed: 300,
-      paginationSpeed: 400,
-      autoPlay: true,
-      stopOnHover: true,
-      singleItem: true,
-      afterInit: '',
-      lazyLoad: true
-    })
-
-    $('.homepage').owlCarousel({
-      navigation: false, // Show next and prev buttons
-      navigationText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
-      slideSpeed: 2000,
-      paginationSpeed: 1000,
-      autoPlay: true,
-      stopOnHover: true,
-      singleItem: true,
-      lazyLoad: false,
-      addClassActive: true,
-      afterInit: function () {
-        // animationsSlider()
-      },
-      afterMove: function () {
-        // animationsSlider()
-      }
-    })
-  }
-}
 
 /* menu sliding */
 function menuSliding () {
@@ -156,65 +24,6 @@ function menuSliding () {
     } else {
       $(this).find('.dropdown-menu').first().stop(true, true).hide()
     }
-  })
-}
-
-/* animations */
-function animations () {
-  var delayTime = 0
-  $('[data-animate]').css({opacity: '0'})
-  $('[data-animate]').waypoint(function () {
-    delayTime += 150
-    $(this).delay(delayTime).queue(function (next) {
-      $(this).toggleClass('animated')
-      $(this).toggleClass($(this).data('animate'))
-      delayTime = 0
-      next()
-      // $(this).removeClass('animated')
-      // $(this).toggleClass($(this).data('animate'))
-    })
-  }, {
-    offset: '90%',
-    triggerOnce: true
-  })
-
-  $('[data-animate-hover]').hover(function () {
-    $(this).css({opacity: 1})
-    $(this).addClass('animated')
-    $(this).removeClass($(this).data('animate'))
-    $(this).addClass($(this).data('animate-hover'))
-  }, function () {
-    $(this).removeClass('animated')
-    $(this).removeClass($(this).data('animate-hover'))
-  })
-}
-
-function animationsSlider () {
-  var delayTimeSlider = 400
-
-  $('.owl-item:not(.active) [data-animate-always]').each(function () {
-    $(this).removeClass('animated')
-    $(this).removeClass($(this).data('animate-always'))
-    $(this).stop(true, true, true).css({opacity: 0})
-  })
-
-  $('.owl-item.active [data-animate-always]').each(function () {
-    delayTimeSlider += 500
-
-    $(this).delay(delayTimeSlider).queue(function () {
-      $(this).addClass('animated')
-      $(this).addClass($(this).data('animate-always'))
-
-      console.log($(this).data('animate-always'))
-    })
-  })
-}
-
-/* counters */
-function counters () {
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
   })
 }
 
@@ -257,38 +66,6 @@ function utils () {
   $('.box.clickable').on('click', function () {
     window.location = $(this).find('a').attr('href')
   })
-
-  /* external links in new window */
-  $('.external').on('click', function (e) {
-    e.preventDefault()
-    window.open($(this).attr('href'))
-  })
-
-  /* animated scrolling */
-  $('.scroll-to, .scroll-to-top').click(function (event) {
-    var fullUrl = this.href
-    var parts = fullUrl.split('#')
-
-    if (parts.length > 1) {
-      scrollTo(fullUrl)
-      event.preventDefault()
-    }
-  })
-
-  function scrollTo (fullUrl) {
-    var parts = fullUrl.split('#')
-    var trgt = parts[1]
-    var targetOffset = $('#' + trgt).offset()
-    var targetTop = targetOffset.top - 100
-
-    if (targetTop < 0) {
-      targetTop = 0
-    }
-
-    $('html, body').animate({
-      scrollTop: targetTop
-    }, 1000)
-  }
 }
 
 /* product detail gallery */
