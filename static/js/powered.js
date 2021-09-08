a1255c1126cf0ad4b7c3519a230a25d7d6fa0b5d
Lelies-L <969966273@qq.com>
2021-09-08 21:56:13 +0800

    replace md file to yaml file

--- static/js/powered.js
+++ static/js/powered.js
@@ -6,6 +6,9 @@ $(function($) {
     });
     $('.fixed-nav ul').find('li').on('click', function (e) {
         let target = e.target
+        // 页面结构为 ul > li > a
+        // 为了防止误触
+        // 需要判断点击的 tagName 是否为 A
         if (target.tagName === 'A') {
             $(this).addClass('active').siblings().removeClass('active')
         }
@@ -18,13 +21,13 @@ $(function($) {
             $('.fixed-nav ul li:nth-child(1)').addClass('active').siblings().removeClass('active')
         } else if ((top > 310) && (top < 1250)) {
             $('.fixed-nav ul li:nth-child(2)').addClass('active').siblings().removeClass('active')
-        }  else if ((top > 1250) && (top < 1360)) {
+        }  else if ((top >= 1250) && (top < 1360)) {
             $('.fixed-nav ul li:nth-child(3)').addClass('active').siblings().removeClass('active')
-        }  else if ((top > 1360) && (top < 1650)) {
+        }  else if ((top >= 1360) && (top < 1650)) {
             $('.fixed-nav ul li:nth-child(4)').addClass('active').siblings().removeClass('active')
-        }  else if ((top > 1650) && (top < 1880)) {
+        }  else if ((top >= 1650) && (top < 1880)) {
             $('.fixed-nav ul li:nth-child(5)').addClass('active').siblings().removeClass('active')
-        }  else if (top > 1880) {
+        }  else if (top >= 1880) {
             $('.fixed-nav ul li:nth-child(6)').addClass('active').siblings().removeClass('active')
         }
     })
